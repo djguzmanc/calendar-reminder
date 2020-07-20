336fe6997c32d433c920b94a48c86128ce8dc5dc
David <djguzmanc@unal.edu.co>
2020-07-20 18:48:39 -0500

    Modules splitting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
 import { CalendarDayComponent } from './calendar/calendar-day/calendar-day.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material/dialog';
-import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 import { ReminderEditorComponent } from './calendar/reminder-editor/reminder-editor.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 import { ReminderComponent } from './calendar/calendar-day/reminder/reminder.component';
 import { SharedPipesModule } from './pipes/shared-pipes.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './custom-modules/material.module';
+import { StoreNgrxModule } from './custom-modules/store.module';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatIconModule } from '@angular/material/icon';
 
 /**
  * Main module
@@ -35,28 +26,15 @@ import { MatIconModule } from '@angular/material/icon';
   ],
   imports: [
     BrowserModule,
-    MatTooltipModule,
-    MatIconModule,
     PerfectScrollbarModule,
+    MaterialModule,
     SharedPipesModule,
     HttpClientModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatInputModule,
-    NgxMaterialTimepickerModule.setLocale('es-CO'),
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true,
-      }
-    }),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
+    StoreNgrxModule
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
